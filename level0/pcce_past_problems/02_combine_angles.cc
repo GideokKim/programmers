4e1900c7ea75c9bc591d1573fef575c174e9cede
gideok Kim <ikid4127@naver.com>
2024-08-11 17:26:37 +0900

    feat: implement pcce past problem 02

---
+++ level0/pcce_past_problems/02_combine_angles.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+  int angle1;
+  int angle2;
+  std::cin >> angle1 >> angle2;
+
+  int sum_angle = angle1 + angle2;
+  std::cout << sum_angle % 360 << std::endl;
+  return 0;
+}
