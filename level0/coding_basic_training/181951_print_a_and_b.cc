69dd98cdb41d92dbeda3a0fb5a72c3841c3844fb
gideok Kim <ikid4127@naver.com>
2024-09-11 23:09:50 +0900

    feat: implement programmers 181951 print a and b

---
+++ level0/coding_basic_training/181951_print_a_and_b.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void) {
+  int a;
+  int b;
+  std::cin >> a >> b;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "b = " << b << std::endl;
+  return 0;
+}
