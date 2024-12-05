6b355b54fd526507176fa3fe31f47ba480b6739a
gideok Kim <ikid4127@naver.com>
2024-12-05 23:58:37 +0900

    feat: implement 181879

---
+++ level0/coding_basic_training/181879_operations_based_on_length.cc
@@ -0,0 +1,16 @@
+#include <vector>
+
+int solution(std::vector<int> num_list) {
+  int answer = 0;
+  if (num_list.size() > 10) {
+    for (auto num : num_list) {
+      answer += num;
+    }
+  } else {
+    answer = 1;
+    for (auto num : num_list) {
+      answer *= num;
+    }
+  }
+  return answer;
+}
